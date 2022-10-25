8c6107445d0eb0cc14baabd959e0600a0f025db0
Tseing <im.yczeng@foxmail.com>
2022-10-25 21:15:05 +0800

    ch04

--- common/layers.py
+++ common/layers.py
@@ -101,3 +101,45 @@ class SoftmaxWithLoss:
         dx = dx / batch_size
 
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0 # 元素为 0
+        # for i, word_id in enumerate(self.idx):
+        #     dW[word_id] += dout[i]
+        # numpy 速度更快
+        np.add.at(dW, self.idx, dout)
+        return None
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoid 的输出
+        self.t = None  # 监督标签
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+        # 交叉熵误差 L = - [t log(y) + (1-t)log(1-y)]
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
